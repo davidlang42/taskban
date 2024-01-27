1120eb23f6202ef3e91398e14fa8c6765d6adfec
David Lang <davidlang42@gmail.com>
2024-01-27 21:27:18 +1100

    Implement import/export UI

---
+++ app/ImportExport.js
@@ -0,0 +1,12 @@
+// Module contains all the code for importing and exporting a full board of tasks from/to CSV
+
+//client call
+function exportToCsv(boardId) {
+  //TODO
+  return "exported,csv,data,is,here";
+}
+
+function importFromCsv(boardId, csvFile) {
+  //TODO
+  return "Successfully imported csv file: " + boardId + " = " + csvFile;
+}
